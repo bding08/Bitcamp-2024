0df825287869e87b211936bfb48eaadde5f97538
Brian Ding <bding08@gmail.com>
2024-04-19 23:55:12 -0400

    finished interestBtn and dynamic highlighting

--- src/app/components/InterestsBtn.tsx
+++ src/app/components/InterestsBtn.tsx
@@ -1,4 +1,6 @@
-import React, { FC } from "react";
+"use client";
+
+import React, { FC, useState } from "react";
 import { Badge } from "@/app/components/ui/badge";
 import { Button } from "./ui/button";
 
@@ -6,11 +8,32 @@ interface InterestProp {
   name: string;
 }
 
+type variant =
+  | "default"
+  | "destructive"
+  | "outline"
+  | "secondary"
+  | "ghost"
+  | "link";
+
 const InterestsBtn: FC<InterestProp> = (prop: InterestProp) => {
+  const [btnClass, setBtnClass] = useState("outline");
+
+  const handleClick = () => {
+    const newVariant = btnClass === "secondary" ? "outline" : "secondary";
+    setBtnClass(newVariant);
+  };
+
   return (
-    <div>
+    <div className="flex justify-center">
       {" "}
-      <Button>{prop.name}</Button>{" "}
+      <Button
+        className="text-lg w-full"
+        onClick={handleClick}
+        variant={btnClass as variant}
+      >
+        {prop.name}
+      </Button>{" "}
     </div>
   );
 };
--- src/app/interests/page.tsx
+++ src/app/interests/page.tsx
@@ -5,14 +5,13 @@ import InterestsBtn from "../components/InterestsBtn";
 import { Button } from "../components/ui/button";
 import { VariantProps } from "class-variance-authority";
 
-interface variant {
-  default: "bg-primary text-primary-foreground hover:bg-primary/90";
-  destructive: "bg-destructive text-destructive-foreground hover:bg-destructive/90";
-  outline: "border border-input bg-background hover:bg-accent hover:text-accent-foreground";
-  secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80";
-  ghost: "hover:bg-accent hover:text-accent-foreground";
-  link: "text-primary underline-offset-4 hover:underline";
-}
+type variant =
+  | "default"
+  | "destructive"
+  | "outline"
+  | "secondary"
+  | "ghost"
+  | "link";
 
 const setupRoute = () => {
   const [btnClass, setBtnClass] = useState("secondary");
@@ -53,12 +52,9 @@ const setupRoute = () => {
     <div className="text-3xl space-y-5 absolute inset-x-0 top-20 ">
       <h1 className="flex items-center justify-center ">Setup Page</h1>
 
-      <div className="grid space-x-2 items-center justify-center  grid-cols-4 gap-3 container px10">
+      <div className="grid items-center justify-center grid-cols-6 w-full gap-3 container px10">
         {interestList.map((interestItem) => (
-          //{btnClass as variant}>
-          <Button onClick={handleClick} variant="secondary">
-            {interestItem}
-          </Button>
+          <InterestsBtn name={interestItem} />
         ))}
       </div>
     </div>
