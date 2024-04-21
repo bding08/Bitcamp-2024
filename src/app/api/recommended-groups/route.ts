8683dc895d62ac2f74e5ac4900a877a6fbabca6e
DaveSan2021 <davayansa@gmail.com>
2024-04-20 23:24:33 -0400

    Added functionality for recomending groups using gemini

--- src/app/api/recommended-groups/route.ts
+++ src/app/api/recommended-groups/route.ts
@@ -10,11 +10,21 @@ const groupSchema = z.object({
     .email("Invalid Email"),
 });
 
+const recommendationSchema = z.object({
+  email: z
+    .string()
+    .min(1, { message: "Email is Required" })
+    .email("Invalid Email"),
+  eventIDs: z.array(z.number()),
+});
+
+//gets all events
 export async function GET() {
   try {
     const events = await db.event.findMany({
         select: {
           title: true,
+          eventID: true,
         },
       },
     );
@@ -30,7 +40,7 @@ export async function POST(req: Request) {
   try {
     
     const body = await req.json();
-    const { email } = groupSchema.parse(body);
+    const { email} = groupSchema.parse(body);
 
     // Query to fetch group information and related events
 
@@ -65,5 +75,57 @@ export async function POST(req: Request) {
   }
     catch (error) {
     console.log("Error in post");
+  }
+}
+
+export async function PUT(req: Request) {
+  try {
+    
+    const body = await req.json();
+    const { email, eventIDs } = recommendationSchema.parse(body);
+
+    // Query to fetch group information and related events
+
+    const user = await db.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    // Step 2: Find the groups that have the specified eventIDs
+    const groups = await db.group.findMany({
+      where: {
+        eventID: {
+          in: eventIDs,
+        },
+      },
+    });
+
+    if (groups.length === 0) {
+      throw new Error('No groups found with the specified eventIDs');
+    }
+    
+    const groupsArray = groups.map((item: { groupID: number, eventID: number }) => ({ groupID: item.groupID }));
+    
+    await db.user.update({
+      where: {
+        id: user.id,
+      },
+      data: {
+        recGroups: {
+          connect: groupsArray,
+        },
+      },
+    });
+  
+    return NextResponse.json(groupsArray, { status: 200 });
+  }
+    catch (error) {
+      console.log(error)
+      return NextResponse.json({ status: 501 });
   }
 }
